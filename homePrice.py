1131dbda75401fd39b465fd5b401e6a02ed04603
Oğuzhan Erbil <82578095+oguzhanerbil@users.noreply.github.com>
2024-01-20 10:30:28 +0300

    .

--- homePrice.py
+++ homePrice.py
@@ -2,30 +2,34 @@ import time
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Web sürücüsünü yükleyin
 driver = webdriver.Chrome()
-
 driver.get("https://www.emlakjet.com/satilik-konut/kocaeli/")
-i = 1
-while i <= 2:
-    classAdress = driver.find_element(By.XPATH,"//*[@id='listing-search-wrapper']")
-    classes = driver.find_elements(By.CLASS_NAME,"_3qUI9q")
-    for tikla in classes:
-        sleep(5)
-        tikla.click()
-        elements = driver.find_elements(By.XPATH,"//*[@id='bilgiler']/div/div[2]/div/div[1]/div[1]")
-        fiyat = driver.find_elements(By.CLASS_NAME,"_2TxNQv")
-        detaylar = []
-        for i in fiyat:
-            fiyat = i.text
-        print("LAAAN"+fiyat)
-        for i in elements:
-            print(i.text)
-            detaylar.append(i.text)
-        driver.execute_script("window.history.go(-1)")
 
+max_pages = 2
+current_page = 1
 
+while current_page <= max_pages:
+    listings = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "_3qUI9q"))
+    )
+    for index in range(len(listings)):
+        listings[index].click()
+        fiyat_elements = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "_2TxNQv"))
+        )
+        info_elements = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//*[@id='bilgiler']/div/div[2]/div/div[1]/div[1]"))
+        )
+        for fiyat_element in fiyat_elements:
+            print(fiyat_element.text)
+        for info_element in info_elements:
+            print(info_element.text)
+        
+        driver.back()
+        
 
 
 driver.close()
