571e99d6abb3593414a3ae44dad8c08d4d38fb62
Oğuzhan Erbil <82578095+oguzhanerbil@users.noreply.github.com>
2024-01-21 18:42:28 +0300

    .

--- homePrice.py
+++ homePrice.py
@@ -9,16 +9,6 @@ import os
 options = webdriver.ChromeOptions()
 options.headless = True
 options.add_argument(f'user-agent={"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/120.0.6099.225 Safari/537.36"}')
-options.add_argument("--window-size=1920,1080")
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--allow-running-insecure-content')
-options.add_argument("--disable-extensions")
-options.add_argument("--proxy-server='direct://'")
-options.add_argument("--proxy-bypass-list=*")
-options.add_argument("--start-maximized")
-options.add_argument('--disable-gpu')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--no-sandbox')
 options.add_argument("--headless")
 
 
